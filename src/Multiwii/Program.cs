bd004ade38fa4381215ffc99f83f2a37bb991dd2
Ladislav Schumacher <ladislav.schumacher4@gmail.com>
2018-11-23 14:49:10 +0100

    Init project

---
+++ src/Multiwii/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multiwii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
